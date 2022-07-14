27ed210772346f4746de1b35c483072d4207a180
MaximKharin <maximkharin@gmail.com>
2022-07-14 17:00:21 +0200

    almost finished task1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Есть монотонная последовательность, каждое число встречается ровно то количество раз,
+// какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,...
+// Выведите ряд этой последовательности до N-го значения, желательно использовать рекурсию:
+//N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5"
+
+int number = Prompt("Enter your number: ");
+Print(number);
+Console.WriteLine();
+
+void Print(int number)
+{
+    if(number > 1)
+        Print(number - 1);
+ 
+    for (int i = 0; i < number; i++) 
+        Console.Write($"{number}, ");
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
